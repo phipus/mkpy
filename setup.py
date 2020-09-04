d91ace2dd2f66ceabcebe0a539a7e2699e554960
phipus <phipuspam@gmx.ch>
2020-09-04 16:11:18 +0200

    Add homepage information

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ from setuptools import setup
 setup(
     name="mkpy",
     author="phipus",
+    author_email="phipuspam@gmx.ch",
+    description="A (minimalistic) build tool simmilar to make. Written in Python 3",
+    url="https://github.com/phipus/mkpy",
     packages=["mkpy"],
     version="0.0.2",
-    entry_points={"console_scripts": "mkpy=mkpy.make:main"}
+    entry_points={"console_scripts": "mkpy=mkpy.make:main"},
 )
